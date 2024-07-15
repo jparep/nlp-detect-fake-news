f8fe7444bcf959c11befb6240d818e661c21e284
jparep <parepson@gmail.com>
2024-07-15 14:43:07 -0600

    Imported necessry libraries

---
+++ ml/scripts/prepare_data.py
@@ -0,0 +1,19 @@
+# Import necessary libraries
+import numpy as np
+import pandas as pd
+import config
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.model_selection import train_test_split
+
+# Download necessary NLTK data
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
+
+
+
+
