6facfb6572c8a09201970b1446b3e9c34abc2fa8
jparep <parepson@gmail.com>
2024-07-15 15:00:27 -0600

    Preprocess data

--- ml/scripts/prepare_data.py
+++ ml/scripts/prepare_data.py
@@ -18,5 +18,20 @@ from sklearn.model_selection import train_test_split
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
+# Set randomsed for reproducibility
+np.random.seed(config.RANDOM_SEED)
+
+# Load Data
+def load_data(real_csv, fake_csv):
+    """Load and concatenate real and fake news data."""
+    df_real = pd.read_csv(real_csv)
+    df_fake = pd.read_csv(fake_csv)
+    df_combine = pd.concat([df_real, df_fake], axis=0).sample(frac=1).reset_index(drop=True)
+    return df_combine
+
+# Preprocess data
+def preprocess_data(text):
+    """Preprocess text data: remove non-alphaberical characters, lowercase, tokenize, lemmatize, remove stopwords"""
+    
 
 
