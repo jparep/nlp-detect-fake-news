c3135f89cc0ccabbe9dc6993b7439854b3e70b1a
jparep <parepson@gmail.com>
2024-07-15 14:29:02 -0600

    fiel directory added

--- ml/scripts/config.py
+++ ml/scripts/config.py
@@ -3,4 +3,5 @@ import os
 # Set the base directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#
+# Path to data files
+FAKE_CSV_PATH = os.path.join(BASE_DIR, 'data', 'raw', 'fake.csv')
