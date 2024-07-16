6f3167e071b231373513a45a5d83a2bde1d9efec
jparep <parepson@gmail.com>
2024-07-15 21:47:45 -0600

    Updated training model

--- ml/scripts/training_model.py
+++ ml/scripts/training_model.py
@@ -1,20 +1,10 @@
 # Import necessary libraries
-from sklearn.feature_extraction.text import TfidfVectorizer
+import os
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 import pickle
 import config
-from data_preparation import load_and_preprocess_data, train_valid_test_split
-
-def vectorize_data(X_train, X_valid, X_test, vectorizer_path):
-    """Vectorize the text data using TF-IDF."""
-    vectorizer = TfidfVectorizer()
-    xv_train = vectorizer.fit_transform(X_train)
-    xv_valid = vectorizer.transform(X_valid)  # Corrected to use transform
-    xv_test = vectorizer.transform(X_test)    # Corrected to use transform
-    with open(vectorizer_path, 'wb') as f:
-        pickle.dump(vectorizer, f)
-    return xv_train, xv_valid, xv_test, vectorizer
+from utils import load_and_preprocess_data, train_valid_test_split, vectorize_data
 
 def train_model(X_train, y_train, model):
     """Train the model."""
@@ -48,7 +38,7 @@ def main():
     for name, model in models.items():
         print(f"Training {name}...")
         model_trained = train_model(xv_train, y_train, model)
-        save_model(model_trained, f"/home/jparep/proj/nlp-detect-fake-news/ml/model/{name.replace(' ', '_').lower()}.pkl")
+        save_model(model_trained, os.path.join(config.BASE_DIR, 'ml', 'model', f"{name.replace(' ', '_').lower()}.pkl"))
 
 if __name__ == "__main__":
     main()
