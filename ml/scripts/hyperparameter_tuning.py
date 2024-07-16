e1ca64c58d9f1c6cef64b703fbbbb6a78210071d
jparep <parepson@gmail.com>
2024-07-15 21:20:19 -0600

    RandomizeSearch created

--- ml/scripts/hyperparameter_tuning.py
+++ ml/scripts/hyperparameter_tuning.py
@@ -14,3 +14,7 @@ def hyperparameter_tuning(X_train, y_train):
         'classifier_max_depth': [None, 10, 30, 60],
         'classifier_min_sample_split': [2, 5, 9, 11]
     }
+    
+    model = RandomizedSearchCV(pipline, param_distributions=param_dist, n_iter=12, cv=10, n_jobs=-1, verbose=1, random_state=config.RANDOM_SEED)
+    model.fit(X_train, y_train)
+    return model.best_estimator_
